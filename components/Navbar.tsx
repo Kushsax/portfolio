5fbf0ea2a192172870c7393eada82a20df75b787
kushsax <kushagrasaxenaa616@gmail.com>
2025-06-11 23:44:25 +0530

    Partial Hero page and Navbar ready

---
+++ components/Navbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+
+
+export default function NavbarDemo() {
+  return (
+    <div className="relative w-full flex items-center justify-center pt- ">
+      <Navbar className="top-8" />
+    </div>
+  );
+}
+ 
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="About">
+          <div className="flex flex-col space-y-4 text-sm">
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Projects">
+          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+            <ProductItem
+              title="LEGO Redesigned"
+              href="https://algochurn.com"
+              src="https://assets.aceternity.com/demos/algochurn.webp"
+              description="A reimagined UI for popular gamemaker LEGO shortlisted top 8 in Mock up 2025"
+            />
+            <ProductItem
+              title="FUNKO"
+              href="https://tailwindmasterkit.com"
+              src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
+              description="Collection of short games like Hangman and Tenzies"
+            />
+            <ProductItem
+              title="Dear Scraps"
+              href="https://gomoonbeam.com"
+              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
+              description="An online Scrapbook making website made for Newbiethon 25"
+            />
+            <ProductItem
+              title="Portfolio Website"
+              href="https://userogue.com"
+              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
+              description="A personal portfolio website to showcase various contributions made by me"
+            />
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Contact">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/hobby">Resume</HoveredLink>
+            <HoveredLink href="https://github.com/Kushsax">GitHub</HoveredLink>
+            <HoveredLink href="https://www.linkedin.com/in/kushsax/">LinkedIn</HoveredLink>
+          </div>
+        </MenuItem>
+      </Menu>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        DEFAULT: 
+      }
+    },
+  },
+  plugins: [
+    // Add any plugins here if needed
+  ],
+}
